d5943e0c872ac2c2be85a2fbb5719ad0e4cd3f10
Jon <jonhelgi@hotmail.com>
2024-03-22 23:32:54 +0000

    Initial commit

--- app/root.tsx
+++ app/root.tsx
@@ -6,9 +6,16 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import type { LinksFunction } from "@remix-run/node";
+import stylesheet from "~/tailwind.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="antialiased">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,324 @@
 import type { MetaFunction } from "@remix-run/node";
+import { BarList, Divider } from "@tremor/react";
+import {
+  Bar,
+  BarChart,
+  Legend,
+  Line,
+  LineChart,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "2024 Pick'Ems 🏆" },
+    { name: "description", content: "2024 PickEms Tournament" },
   ];
 };
 
+const score = [
+  {
+    name: "Start",
+    tttslr: 0,
+    bill: 0,
+    rabbit: 0,
+    b0sskit: 0,
+    ujei: 0,
+    lils: 0,
+    jontrooper: 0,
+  },
+  {
+    name: "Match 1",
+    tttslr: 20,
+    bill: 20,
+    rabbit: 20,
+    b0sskit: 20,
+    ujei: 20,
+    lils: 0,
+    jontrooper: 0,
+  },
+  {
+    name: "Match 2",
+    tttslr: 20,
+    bill: 40,
+    rabbit: 40,
+    b0sskit: 20,
+    ujei: 20,
+    lils: 20,
+    jontrooper: 20,
+  },
+  {
+    name: "Match 3",
+    tttslr: 20,
+    bill: 40,
+    rabbit: 40,
+    b0sskit: 20,
+    ujei: 20,
+    lils: 30,
+    jontrooper: 20,
+  },
+  {
+    name: "Match 4",
+    tttslr: 20,
+    bill: 40,
+    rabbit: 50,
+    b0sskit: 20,
+    ujei: 30,
+    lils: 40,
+    jontrooper: 20,
+  },
+  {
+    name: "Match 5",
+    tttslr: 20,
+    bill: 40,
+    rabbit: 50,
+    b0sskit: 20,
+    ujei: 30,
+    lils: 50,
+    jontrooper: 30,
+  },
+  {
+    name: "Match 6",
+    tttslr: 30,
+    bill: 50,
+    rabbit: 60,
+    b0sskit: 30,
+    ujei: 40,
+    lils: 50,
+    jontrooper: 40,
+  },
+  {
+    name: "Match 7",
+    tttslr: 50,
+    bill: 70,
+    rabbit: 60,
+    b0sskit: 30,
+    ujei: 40,
+    lils: 50,
+    jontrooper: 40,
+  },
+  {
+    name: "Match 8",
+    tttslr: 50,
+    bill: 70,
+    rabbit: 60,
+    b0sskit: 30,
+    ujei: 40,
+    lils: 70,
+    jontrooper: 60,
+  },
+];
+
+const teamData = [
+  {
+    name: "MOUZ",
+    value: 6,
+    icon: function Icon() {
+      return <img src="img/mouz.png" width={19} alt="" className="mr-2" />;
+    },
+    color: "red-600",
+  },
+  {
+    name: "G2",
+    value: 6,
+    icon: function Icon() {
+      return <img src="img/g2.png" width={21} alt="" className="mr-1" />;
+    },
+    color: "red-500",
+  },
+  {
+    name: "Vitality",
+    value: 6,
+    icon: function Icon() {
+      return <img src="img/vitality.png" width={20} alt="" className="mr-1" />;
+    },
+    color: "yellow-300",
+  },
+  {
+    name: "FaZe",
+    value: 5,
+    icon: function Icon() {
+      return <img src="img/faze.png" width={31} alt="" className="mr-1" />;
+    },
+    color: "red-800",
+  },
+  {
+    name: "Mongolz",
+    value: 5,
+    icon: function Icon() {
+      return <img src="img/mongolz.png" width={16} alt="" className="mr-1" />;
+    },
+    color: "stone-500",
+  },
+  {
+    name: "Furia",
+    value: 5,
+    icon: function Icon() {
+      return <img src="img/furia.png" width={21} alt="" className="mr-1" />;
+    },
+    color: "neutral-400",
+  },
+  {
+    name: "TeamSpirit",
+    value: 4,
+    icon: function Icon() {
+      return (
+        <img src="img/teamspirit.png" width={21} alt="" className="mr-1" />
+      );
+    },
+    color: "slate-50",
+  },
+  {
+    name: "Heroic",
+    value: 4,
+    icon: function Icon() {
+      return <img src="img/heroic.png" width={26} alt="" className="mr-1" />;
+    },
+    color: "slate-600",
+  },
+  {
+    name: "NAVI",
+    value: 3,
+    icon: function Icon() {
+      return <img src="img/navi.png" width={26} alt="" className="mr-1" />;
+    },
+    color: "yellow-400",
+  },
+  {
+    name: "VP",
+    value: 3,
+    icon: function Icon() {
+      return <img src="img/vp.png" width={21} alt="" className="mr-1" />;
+    },
+    color: "orange-500",
+  },
+  {
+    name: "Complexity",
+    value: 2,
+    icon: function Icon() {
+      return <img src="img/comp.png" width={24} alt="" className="mr-1" />;
+    },
+    color: "blue-300",
+  },
+  {
+    name: "EF",
+    value: 2,
+    icon: function Icon() {
+      return <img src="img/eternal.png" width={38} alt="" className="mr-1" />;
+    },
+    color: "orange-100",
+  },
+  {
+    name: "Paingaming",
+    value: 2,
+    icon: function Icon() {
+      return <img src="img/pain.png" width={31} alt="" className="mr-1" />;
+    },
+    color: "pink-500",
+  },
+  {
+    name: "Esc",
+    value: 2,
+    icon: function Icon() {
+      return <img src="img/esc.png" width={21} alt="" className="mr-1" />;
+    },
+    color: "orange-300",
+  },
+  {
+    name: "C9",
+    value: 1,
+    icon: function Icon() {
+      return <img src="img/c9.png" width={30} alt="" className="mr-1" />;
+    },
+    color: "blue-500",
+  },
+  {
+    name: "Imperial",
+    value: 1,
+    icon: function Icon() {
+      return <img src="img/imp.png" width={20} alt="" className="mr-1" />;
+    },
+    color: "green-500",
+  },
+];
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="max-w-2xl m-auto">
+      <div className="m-4">
+        <h1 className="m-auto w-max font-medium font-display text-3xl drop-shadow-md">
+          🏆 CS2 Copenhagen 2024 Pick'Em Stats 📊
+        </h1>
+        <h3 className="m-auto w-max font-medium font-display drop-shadow-md">
+          Winner Takes All
+        </h3>
+        <div className="mb-4">
+          <ResponsiveContainer width="99%" height={360}>
+            <LineChart width={600} height={400} data={score}>
+              <Line
+                type="basis"
+                dataKey="tttslr"
+                stroke="#ffffff"
+                strokeWidth={3}
+                dot={false}
+              />
+              <Line
+                name="Bill"
+                type="basis"
+                dataKey="bill"
+                stroke="#7d8174"
+                strokeWidth={3}
+                dot={false}
+              />
+              <Line
+                type="basis"
+                dataKey="rabbit"
+                stroke="#2ecc71"
+                strokeWidth={3}
+                dot={false}
+              />
+              <Line
+                name="B0sskit"
+                type="basis"
+                dataKey="b0sskit"
+                stroke="#e2be74"
+                strokeWidth={3}
+                dot={false}
+              />
+              <Line
+                type="basis"
+                dataKey="ujei"
+                stroke="#9ed1e1"
+                strokeWidth={3}
+                dot={false}
+              />
+              <Line
+                name="Lils"
+                type="basis"
+                dataKey="lils"
+                stroke="#8a7b64"
+                strokeWidth={3}
+                dot={false}
+              />
+              <Line
+                name="JonTrooper"
+                type="basis"
+                dataKey="jontrooper"
+                stroke="#fee5b9"
+                strokeWidth={3}
+                dot={false}
+              />
+              <Legend />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <Divider>Team Picks</Divider>
+        <BarList data={teamData} />
+      </div>
     </div>
   );
 }
