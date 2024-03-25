1f5278ec006f162b7387c2625be19c4e6f9d1fe9
Jon <jonhelgi@hotmail.com>
2024-03-25 15:33:25 +0000

    Update Leaderboard component styling

--- app/components/Leaderboard.tsx
+++ app/components/Leaderboard.tsx
@@ -4,7 +4,7 @@ import { Badge, BadgeDelta, Card, SparkAreaChart } from "@tremor/react";
 export default function Leaderboard({ data }: { data: any[] }) {
   return (
     <div className="max-w-md m-auto">
-      <Card className="mx-auto flex items-center justify-between px-2.5 py-1 dark:bg-neutral-950 hover:dark:bg-neutral-900 transition-all">
+      <Card className="mx-auto flex items-center justify-between px-1 py-1 dark:bg-neutral-950 hover:dark:bg-neutral-900 transition-all">
         <div className="flex items-center space-x-2.5">
           <img
             src="img/avatars/tttslr.png"
@@ -16,19 +16,12 @@ export default function Leaderboard({ data }: { data: any[] }) {
           </p>
         </div>
         <div className="flex items-center space-x-2.5">
-          <span className="font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">
-            {data[data.length - 1].tttslr * 0.1}pts
-          </span>
-          <BadgeDelta
-            deltaType="unchanged"
-            isIncreasePositive={true}
-            className="grayscale"
-          >
-            0
-          </BadgeDelta>
+          <Badge className="font-medium" color={"emerald-500"} size={"lg"}>
+            {data[data.length - 1].tttslr * 0.1}
+          </Badge>
         </div>
       </Card>
-      <Card className="mx-auto flex max-w-lg items-center justify-between px-2.5 py-1 my-1.5 dark:bg-neutral-950 hover:dark:bg-neutral-900 transition-all">
+      <Card className="mx-auto flex max-w-lg items-center justify-between px-1 py-1 my-1.5 dark:bg-neutral-950 hover:dark:bg-neutral-900 transition-all">
         <div className="flex items-center space-x-2.5">
           <img
             src="img/avatars/b0sskit.png"
@@ -48,19 +41,12 @@ export default function Leaderboard({ data }: { data: any[] }) {
           </Badge>
         </div>
         <div className="flex items-center space-x-2.5">
-          <span className="font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">
-            {data[data.length - 1].b0sskit * 0.1}pts
-          </span>
-          <BadgeDelta
-            deltaType="unchanged"
-            isIncreasePositive={true}
-            className="grayscale"
-          >
-            0
-          </BadgeDelta>
+          <Badge className="font-medium" color={"emerald-500"} size={"lg"}>
+            {data[data.length - 1].b0sskit * 0.1}
+          </Badge>
         </div>
       </Card>
-      <Card className="mx-auto flex max-w-lg items-center justify-between px-2.5 py-1 my-1.5 dark:bg-neutral-950 hover:dark:bg-neutral-900 transition-all">
+      <Card className="mx-auto flex max-w-lg items-center justify-between px-1 py-1 my-1.5 dark:bg-neutral-950 hover:dark:bg-neutral-900 transition-all">
         <div className="flex items-center space-x-2.5">
           <img
             src="img/avatars/rabbit.png"
@@ -72,19 +58,12 @@ export default function Leaderboard({ data }: { data: any[] }) {
           </p>
         </div>
         <div className="flex items-center space-x-2.5">
-          <span className="font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">
-            {data[data.length - 1].rabbit * 0.1}pts
-          </span>
-          <BadgeDelta
-            deltaType="unchanged"
-            isIncreasePositive={true}
-            className="grayscale"
-          >
-            0
-          </BadgeDelta>
+          <Badge className="font-medium" color={"emerald-300"} size={"lg"}>
+            {data[data.length - 1].rabbit * 0.1}
+          </Badge>
         </div>
       </Card>
-      <Card className="mx-auto flex max-w-lg items-center justify-between px-2.5 py-1 my-1.5 dark:bg-neutral-950 hover:dark:bg-neutral-900 transition-all">
+      <Card className="mx-auto flex max-w-lg items-center justify-between px-1 py-1 my-1.5 dark:bg-neutral-950 hover:dark:bg-neutral-900 transition-all">
         <div className="flex items-center space-x-2.5">
           <img
             src="img/avatars/bill.png"
@@ -96,19 +75,12 @@ export default function Leaderboard({ data }: { data: any[] }) {
           </p>
         </div>
         <div className="flex items-center space-x-2.5">
-          <span className="font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">
-            {data[data.length - 1].bill * 0.1}pts
-          </span>
-          <BadgeDelta
-            deltaType="unchanged"
-            isIncreasePositive={true}
-            className="grayscale"
-          >
-            0
-          </BadgeDelta>
+          <Badge className="font-medium" color={"emerald-200"} size={"lg"}>
+            {data[data.length - 1].bill * 0.1}
+          </Badge>
         </div>
       </Card>
-      <Card className="mx-auto flex max-w-lg items-center justify-between px-2.5 py-1 my-1.5 dark:bg-neutral-950 hover:dark:bg-neutral-900 transition-all">
+      <Card className="mx-auto flex max-w-lg items-center justify-between px-1 py-1 my-1.5 dark:bg-neutral-950 hover:dark:bg-neutral-900 transition-all">
         <div className="flex items-center space-x-2.5">
           <img
             src="img/avatars/ujei.png"
@@ -120,19 +92,12 @@ export default function Leaderboard({ data }: { data: any[] }) {
           </p>
         </div>
         <div className="flex items-center space-x-2.5">
-          <span className="font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">
-            {data[data.length - 1].ujei * 0.1}pts
-          </span>
-          <BadgeDelta
-            deltaType="unchanged"
-            isIncreasePositive={true}
-            className="grayscale"
-          >
-            0
-          </BadgeDelta>
+          <Badge className="font-medium" color={"emerald-100"} size={"lg"}>
+            {data[data.length - 1].ujei * 0.1}
+          </Badge>
         </div>
       </Card>
-      <Card className="mx-auto flex max-w-lg items-center justify-between px-2.5 py-1 my-1.5 dark:bg-neutral-950 hover:dark:bg-neutral-900 transition-all">
+      <Card className="mx-auto flex max-w-lg items-center justify-between px-1 py-1 my-1.5 dark:bg-neutral-950 hover:dark:bg-neutral-900 transition-all">
         <div className="flex items-center space-x-2.5">
           <img
             src="img/avatars/jontrooper.png"
@@ -144,19 +109,12 @@ export default function Leaderboard({ data }: { data: any[] }) {
           </p>
         </div>
         <div className="flex items-center space-x-2.5">
-          <span className="font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">
-            {data[data.length - 1].jontrooper * 0.1}pts
-          </span>
-          <BadgeDelta
-            deltaType="unchanged"
-            isIncreasePositive={true}
-            className="grayscale"
-          >
-            0
-          </BadgeDelta>
+          <Badge className="font-medium" color={"emerald-100"} size={"lg"}>
+            {data[data.length - 1].jontrooper * 0.1}
+          </Badge>
         </div>
       </Card>
-      <Card className="mx-auto flex max-w-lg items-center justify-between px-2.5 py-1 my-1.5 dark:bg-neutral-950 hover:dark:bg-neutral-900 transition-all">
+      <Card className="mx-auto flex max-w-lg items-center justify-between px-1 py-1 my-1.5 dark:bg-neutral-950 hover:dark:bg-neutral-900 transition-all">
         <div className="flex items-center space-x-2.5">
           <img
             src="img/avatars/lils.png"
@@ -168,16 +126,9 @@ export default function Leaderboard({ data }: { data: any[] }) {
           </p>
         </div>
         <div className="flex items-center space-x-2.5">
-          <span className="font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">
-            {data[data.length - 1].lils * 0.1}pts
-          </span>
-          <BadgeDelta
-            deltaType="unchanged"
-            isIncreasePositive={true}
-            className="grayscale"
-          >
-            0
-          </BadgeDelta>
+          <Badge className="font-medium" color={"emerald-50"} size={"lg"}>
+            {data[data.length - 1].lils * 0.1}
+          </Badge>
         </div>
       </Card>
     </div>
