e0e95d229d72ad1a3b7a50a021055e635152451a
Jon <jonhelgi@hotmail.com>
2024-03-31 16:50:07 +0000

    Fix typo in index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="text-3xl">Scored are being tallied!</div>
+      <div className="text-3xl">Scores are being tallied!</div>
     </div>
   );
 }
