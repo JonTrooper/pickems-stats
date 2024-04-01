f588e88c4857fa0c0e2de8adad6b11baf124a604
Jon <jonhelgi@hotmail.com>
2024-04-01 17:43:24 +0000

    Update final results

--- app/components/Leaderboard.tsx
+++ app/components/Leaderboard.tsx
@@ -1,18 +1,11 @@
 import {
-  RiArrowDownDoubleLine,
-  RiArrowDownFill,
-  RiArrowDownSFill,
-  RiArrowDownSLine,
-  RiArrowUpCircleLine,
-  RiArrowUpDoubleLine,
-  RiArrowUpFill,
-  RiArrowUpSLine,
   RiCrosshair2Fill,
-  RiFireFill,
+  RiMedalFill,
+  RiShieldCheckFill,
   RiSwordFill,
-  RiVipCrownFill,
+  RiTrophyFill,
 } from "@remixicon/react";
-import { Badge, BadgeDelta, Card, SparkAreaChart } from "@tremor/react";
+import { Badge, Card } from "@tremor/react";
 
 export default function Leaderboard({ data }: { data: any[] }) {
   return (
@@ -22,12 +15,10 @@ export default function Leaderboard({ data }: { data: any[] }) {
           <img
             src="img/avatars/jontrooper.png"
             width={34}
-            className="mr-1 rounded-full"
+            className="mr-0.5 rounded-full"
           />
-          <p className="font-extrabold bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.orange.500),theme(colors.orange.200),theme(colors.amber.400),theme(colors.orange.500))] bg-[length:200%_auto] animate-gradient px-1">
-            JonTrooper
-          </p>
-          <Badge
+          <p className="text-jontrooper font-medium px-1">JonTrooper</p>
+          {/* <Badge
             color={"orange-400"}
             icon={RiFireFill}
             tooltip="7 Pick Winstreak!"
@@ -35,7 +26,7 @@ export default function Leaderboard({ data }: { data: any[] }) {
             size={"xs"}
           >
             On Fire!
-          </Badge>
+          </Badge> */}
           <Badge
             color={"rose-500"}
             icon={RiSwordFill}
@@ -47,23 +38,129 @@ export default function Leaderboard({ data }: { data: any[] }) {
           <Badge
             color={"amber-200"}
             icon={RiCrosshair2Fill}
-            tooltip="Day 4-5"
+            tooltip="Champions Stage"
             size={"xs"}
           >
-            4-0
+            7-0
           </Badge>
         </div>
         <div className="flex items-center space-x-1">
-          <div className="text-emerald-400">
-            {/* <RiArrowUpSLine /> */}
-            {/* <RiArrowUpDoubleLine /> */}
+          <div className="mr-2 flex items-center">
+            <img src="img/awards/1st.png" width={33} />
+            <Badge
+              color={"yellow-300"}
+              icon={RiTrophyFill}
+              tooltip="CS2: Copenhagen Major 2024 Pick'Ems Winner"
+              className="ml-1 pl-3"
+              size={"xs"}
+            >
+              1st
+            </Badge>
           </div>
           <Badge
             className="font-mono font-bold py-1"
             color={"emerald-500"}
             size={"md"}
           >
-            {data[data.length - 1].jontrooper * 0.1}
+            {data[data.length - 1].jontrooper}
+          </Badge>
+        </div>
+      </Card>
+      <Card className="mx-auto flex items-center justify-between px-1 py-1 dark:bg-neutral-950 hover:dark:bg-[#0e0e0e] transition-all">
+        <div className="flex items-center space-x-1">
+          <img
+            src="img/avatars/lils.png"
+            width={34}
+            className="mr-0.5 rounded-full"
+          />
+          <p className="text-lils font-medium px-1">Lils</p>
+          <Badge
+            color={"sky-500"}
+            icon={RiSwordFill}
+            tooltip="Picked Cloud9 to win G2 on Day 1"
+            className="ml-2"
+            size={"xs"}
+          >
+            Clutch
+          </Badge>
+          <Badge
+            color={"amber-200"}
+            icon={RiCrosshair2Fill}
+            tooltip="Day 6-7"
+            className="ml-2"
+            size={"xs"}
+          >
+            3-0
+          </Badge>
+          <Badge
+            color={"green-400"}
+            icon={RiShieldCheckFill}
+            tooltip="Full bonus points on Day 7"
+            className="ml-2"
+            size={"xs"}
+          >
+            Bonus
+          </Badge>
+        </div>
+        <div className="flex items-center space-x-1">
+          <div className="mr-2 flex items-center">
+            <img src="img/awards/2nd.png" width={33} />
+            <Badge
+              color={"neutral-300"}
+              icon={RiMedalFill}
+              tooltip="CS2: Copenhagen Major Pick'Ems 2024 2nd Place"
+              className="ml-1"
+              size={"xs"}
+            >
+              2nd
+            </Badge>
+          </div>
+          <Badge
+            className="font-mono font-bold py-1"
+            color={"emerald-400"}
+            size={"md"}
+          >
+            {data[data.length - 1].lils}
+          </Badge>
+        </div>
+      </Card>
+      <Card className="mx-auto flex items-center justify-between px-1 py-1 dark:bg-neutral-950 hover:dark:bg-[#0e0e0e] transition-all my-1.5">
+        <div className="flex items-center space-x-1">
+          <img
+            src="img/avatars/tttslr.png"
+            width={34}
+            className="mr-0.5 rounded-full"
+          />
+          <p className="font-medium text-neutral-100 px-1">tttslr</p>
+          <Badge
+            color={"green-400"}
+            icon={RiShieldCheckFill}
+            tooltip="Full bonus points on Day 7"
+            className="ml-2"
+            size={"xs"}
+          >
+            Bonus
+          </Badge>
+        </div>
+        <div className="flex items-center space-x-1">
+          <div className="mr-2 flex items-center">
+            <img src="img/awards/3rd.png" width={33} />
+            <Badge
+              color={"yellow-600"}
+              icon={RiMedalFill}
+              tooltip="CS2: Copenhagen Major Pick'Ems 2024 3rd Place"
+              className="ml-1.5"
+              size={"xs"}
+            >
+              3rd
+            </Badge>
+          </div>
+          <Badge
+            className="font-mono font-bold py-1"
+            color={"emerald-400"}
+            size={"md"}
+          >
+            {data[data.length - 1].tttslr}
           </Badge>
         </div>
       </Card>
@@ -72,7 +169,7 @@ export default function Leaderboard({ data }: { data: any[] }) {
           <img
             src="img/avatars/b0sskit.png"
             width={34}
-            className="mr-1 rounded-full"
+            className="mr-0.5 rounded-full"
           />
           <p className="text-b0sskit font-medium px-1">B0sskit</p>
           <Badge
@@ -92,42 +189,30 @@ export default function Leaderboard({ data }: { data: any[] }) {
           </div>
           <Badge
             className="font-mono font-bold py-1"
-            color={"emerald-400"}
+            color={"emerald-300"}
             size={"md"}
           >
-            {data[data.length - 1].b0sskit * 0.1}
+            {data[data.length - 1].b0sskit}
           </Badge>
         </div>
       </Card>
-      <Card className="mx-auto flex items-center justify-between px-1 py-1 dark:bg-neutral-950 hover:dark:bg-[#0e0e0e] transition-all">
+
+      <Card className="mx-auto flex items-center justify-between px-1 py-1 my-1.5 dark:bg-neutral-950 hover:dark:bg-[#0e0e0e] transition-all">
         <div className="flex items-center space-x-1">
           <img
-            src="img/avatars/lils.png"
+            src="img/avatars/rabbit.png"
             width={34}
-            className="mr-1 rounded-full"
+            className="mr-0.5 rounded-full"
           />
-          <p className="text-lils font-medium px-1">Lils</p>
-          <Badge
-            color={"sky-500"}
-            icon={RiSwordFill}
-            tooltip="Picked Cloud9 to win G2 on Day 1"
-            className="ml-2"
-            size={"xs"}
-          >
-            Clutch
-          </Badge>
+          <p className="font-medium text-rabbit px-1">rabbit</p>
         </div>
         <div className="flex items-center space-x-1">
-          <div className="text-emerald-400">
-            <RiArrowUpSLine />
-            {/* <RiArrowUpDoubleLine /> */}
-          </div>
           <Badge
             className="font-mono font-bold py-1"
-            color={"emerald-400"}
+            color={"emerald-300"}
             size={"md"}
           >
-            {data[data.length - 1].lils * 0.1}
+            {data[data.length - 1].rabbit}
           </Badge>
         </div>
       </Card>
@@ -136,65 +221,17 @@ export default function Leaderboard({ data }: { data: any[] }) {
           <img
             src="img/avatars/bill.png"
             width={34}
-            className="mr-1 rounded-full"
+            className="mr-0.5 rounded-full"
           />
           <p className="text-bill font-medium px-1">Bill</p>
         </div>
         <div className="flex items-center space-x-1">
-          <div className="text-red-400">
-            <RiArrowDownSLine />
-          </div>
           <Badge
             className="py-1 font-mono font-bold"
-            color={"emerald-400"}
-            size={"md"}
-          >
-            {data[data.length - 1].bill * 0.1}
-          </Badge>
-        </div>
-      </Card>
-      <Card className="mx-auto flex items-center justify-between px-1 py-1 dark:bg-neutral-950 hover:dark:bg-[#0e0e0e] transition-all">
-        <div className="flex items-center space-x-1">
-          <img
-            src="img/avatars/tttslr.png"
-            width={34}
-            className="mr-1 rounded-full"
-          />
-          <p className="font-medium text-neutral-100 px-1">tttslr</p>
-        </div>
-        <div className="flex items-center space-x-1">
-          <div className="text-green-400">
-            <RiArrowUpSLine />
-            {/* <RiArrowDownSLine /> */}
-          </div>
-          <Badge
-            className="font-mono font-bold py-1"
-            color={"emerald-300"}
-            size={"md"}
-          >
-            {data[data.length - 1].tttslr * 0.1}
-          </Badge>
-        </div>
-      </Card>
-      <Card className="mx-auto flex items-center justify-between px-1 py-1 my-1.5 dark:bg-neutral-950 hover:dark:bg-[#0e0e0e] transition-all">
-        <div className="flex items-center space-x-1">
-          <img
-            src="img/avatars/rabbit.png"
-            width={34}
-            className="mr-1 rounded-full"
-          />
-          <p className="font-medium text-rabbit px-1">rabbit</p>
-        </div>
-        <div className="flex items-center space-x-1">
-          <div className="text-red-400">
-            <RiArrowDownSLine />
-          </div>
-          <Badge
-            className="font-mono font-bold py-1"
             color={"emerald-200"}
             size={"md"}
           >
-            {data[data.length - 1].rabbit * 0.1}
+            {data[data.length - 1].bill}
           </Badge>
         </div>
       </Card>
@@ -204,18 +241,17 @@ export default function Leaderboard({ data }: { data: any[] }) {
           <img
             src="img/avatars/ujei.png"
             width={34}
-            className="mr-1 rounded-full"
+            className="mr-0.5 rounded-full"
           />
           <p className="text-ujei font-medium px-1">ujei</p>
         </div>
         <div className="flex items-center space-x-1">
-          <div className="text-red-400">{/* <RiArrowDownSLine /> */}</div>
           <Badge
             className="font-mono font-bold py-1"
             color={"emerald-100"}
             size={"md"}
           >
-            {data[data.length - 1].ujei * 0.1}
+            {data[data.length - 1].ujei}
           </Badge>
         </div>
       </Card>
